add8401e756c7e070d965953353d0a707abbbda2
Valdo <vlorian22@GMAIL.COM>
2021-02-23 06:56:36 -0600

    added the waitUntil into self.addEventListener

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,5 +18,10 @@ const FILES_TO_CACHE= [
 ];
 // self keyword to instantiate listeners on the service worker. 
 self.addEventListener('install',function(e){
-
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            console.log('installing cache : '+ CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
 });
