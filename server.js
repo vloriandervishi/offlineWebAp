b69d789878fca33746d66d337bc53820a9ef044e
Valdo <vlorian22@GMAIL.COM>
2021-02-28 12:17:40 -0600

    added comment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+// added stuff
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
